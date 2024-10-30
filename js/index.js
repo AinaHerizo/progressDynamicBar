9b3916252ce5fc6e9f34203b02c815a14a95470f
AinaHerizo <herizoaina09@gmail.com>
2024-10-30 16:58:52 +0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+// Variable
+    // bar
+    const barNumberEcho = document.querySelector("#barNumber");
+    const progressBar = document.querySelector("#progressBar");
+    // form
+    const numberChange = document.querySelector("#numberChange");
+    const changeValue = document.querySelector("#changeValue");
+    // initialisation
+    let count = 0
+    
+
+
+    changeValue.addEventListener("click", (event)=>{
+        event.preventDefault()
+        let utilisateurValue =  Number(numberChange.value)
+        progressBar.style.width = numberChange.value+"%"
+        let timeInterval = 3000/(Math.abs(numberChange.value-count))
+        
+        if(window.barProgression){
+            clearInterval(window.barProgression)
+        }
+        else{
+            window.barProgression = setInterval(() => {
+                if (count < utilisateurValue) {
+                    count++
+                } 
+                else if(count > utilisateurValue) {
+                    count--
+                }
+                else{
+                    clearInterval(barProgression)
+                    barProgression = null;
+                    return;
+                }
+                barNumberEcho.innerHTML = count+"%"                
+            }, timeInterval);
+        }
+    })
+});
